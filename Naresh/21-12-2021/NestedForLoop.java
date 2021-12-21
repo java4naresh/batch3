a5822590777b9223b9108bd4172daeb01fadf1a8
java4naresh <kambalanaresh4java@gmail.com>
2021-12-21 21:17:53 +0530

    21-12-2021 Programs

---
+++ Naresh/21-12-2021/NestedForLoop.java
@@ -0,0 +1,53 @@
+class NestedForLoop 
+{
+	public static void main(String[] args) 
+	{              // 3<6
+		/*for(int i=1; i<6;i++) {
+              // 1<6
+		for(int j=1; j<6;j++) {
+		System.out.println("Naresh");
+		}
+		System.out.println("==============");
+
+		}*/
+		
+		/*for(int i=1; i<6;i++) {
+		for(int j=1; j<11;j++) {
+		System.out.print("*");//***** 
+		}
+		System.out.println();
+		}*/
+
+
+		/*for(int i=1;i<6;i++) {
+		for(int j=1;j<6;j++) {//11111
+		System.out.print(i);
+		}
+		System.out.println();
+		}*/
+
+		/*for(int i=1;i<6;i++) {
+		for(int j=1;j<6;j++) {
+		System.out.print(j);
+		}
+		System.out.println();
+		}*/
+              
+		/*for(int i=1;i<6;i++) {
+		for(int j=1;j<6;j++) {
+			//3 <= 2
+		if(j<=i) System.out.print("*");
+		}
+		System.out.println();
+		}*/
+
+		for(int i=1; i<6;i++) {
+			         
+		for(int j=5; j>0;j--) {
+			// 5 <= 5
+		if(i <= j) System.out.print("*");
+		}
+		System.out.println();
+		}
+	}
+}
