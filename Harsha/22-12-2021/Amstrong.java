e8f65758a46ed5861ebf098cb0c9287492956cb3
java4naresh <kambalanaresh4java@gmail.com>
2021-12-24 21:20:12 +0530

    24-12-2021 ArmStrong Correction

--- Harsha/22-12-2021/Amstrong.java
+++ Harsha/22-12-2021/Amstrong.java
@@ -1,12 +1,13 @@
 class Amstrong{
 public static void main(String[] args){
-int num = 153;
+int num = 152;
+int temp = num;
 int total = 0, sum;
-for(;num!=0;)
-sum = num % 10;
-total = total + sum*sum*sum;
+for(;num!=0;num = num/10) {//153/10 = 15, 15/10 = 1, 1/10 = 0
+int rem = num % 10;//153%10 = 3, 15/%10 = 5, 1 % 10 = 1
+total = total + rem*rem*rem;//27, 125, 1
 }
-if(total == num)
+if(total == temp)
 System.out.println("Amstrong ");
 else
 System.out.println("Not an Amstrong ");
